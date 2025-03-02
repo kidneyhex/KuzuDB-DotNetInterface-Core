9a1cb1ece27854f758455723bf7c4153861b4fb7
Logan B <3870583+thinkpoop@users.noreply.github.com>
2025-03-01 21:13:44 -0600

    Update to KuzuDB 0.8.2; Add .NET Core example

--- KuzuDB-net/ConsoleAppExample/Program.cs
+++ KuzuDB-net/ConsoleAppExample/Program.cs
@@ -9,58 +9,55 @@ namespace ConsoleAppExample
     {
         static void Main(string[] args)
         {
-            kuzu_database db = kuzu_database_init("test", kuzu_default_system_config());
-            kuzu_connection conn = kuzu_connection_init(db);
-
-            kuzu_query_result result;
-
-            result = kuzu_connection_query(conn, "CREATE NODE TABLE User(name STRING, age INT64, PRIMARY KEY (name))");
-            kuzu_query_result_destroy(result);
-            result = kuzu_connection_query(conn, "CREATE NODE TABLE City(name STRING, population INT64, PRIMARY KEY (name))");
-            kuzu_query_result_destroy(result);
-            result = kuzu_connection_query(conn, "CREATE REL TABLE Follows(FROM User TO User, since INT64)");
-            kuzu_query_result_destroy(result);
-            result = kuzu_connection_query(conn, "CREATE REL TABLE LivesIn(FROM User TO City)");
-            kuzu_query_result_destroy(result);
-
-            result = kuzu_connection_query(conn, "COPY User FROM \"csv/users.csv\"");
-            kuzu_query_result_destroy(result);
-            result = kuzu_connection_query(conn, "COPY City FROM \"csv/cities.csv\"");
-            kuzu_query_result_destroy(result);
-            result = kuzu_connection_query(conn, "COPY Follows FROM \"csv/follows.csv\"");
-            kuzu_query_result_destroy(result);
-            result = kuzu_connection_query(conn, "COPY LivesIn FROM \"csv/lives.csv\"");
-            kuzu_query_result_destroy(result);
-
-            result = kuzu_connection_query(conn, "MATCH (a:User)-[f:Follows]->(b:User) RETURN a.name, f.since, b.name;");
-
-            while (kuzu_query_result_has_next(result))
+            using (kuzu_database db = kuzu_database_init("test", kuzu_default_system_config()))
+            using (kuzu_connection conn = kuzu_connection_init(db))
             {
+                PerformNonQuery(conn, "CREATE NODE TABLE User(name STRING, age INT64, PRIMARY KEY (name))");
+                PerformNonQuery(conn, "CREATE NODE TABLE City(name STRING, population INT64, PRIMARY KEY (name))");
+                PerformNonQuery(conn, "CREATE REL TABLE Follows(FROM User TO User, since INT64)");
+                PerformNonQuery(conn, "CREATE REL TABLE LivesIn(FROM User TO City)");
 
-                kuzu_flat_tuple tuple = kuzu_query_result_get_next(result);
-                kuzu_value value;
+                PerformNonQuery(conn, "COPY User FROM \"csv/users.csv\"");
+                PerformNonQuery(conn, "COPY City FROM \"csv/cities.csv\"");
+                PerformNonQuery(conn, "COPY Follows FROM \"csv/follows.csv\"");
+                PerformNonQuery(conn, "COPY LivesIn FROM \"csv/lives.csv\"");
 
-                value = kuzu_flat_tuple_get_value(tuple, 0);
-                String name = kuzu_value_get_string(value);
-                kuzu_value_destroy(value);
+                var result = kuzu_connection_query(conn, "MATCH (a:User)-[f:Follows]->(b:User) RETURN a.name, f.since, b.name;");
 
-                value = kuzu_flat_tuple_get_value(tuple, 1);
-                String since = kuzu_value_get_string(value);
-                kuzu_value_destroy(value);
+                while (kuzu_query_result_has_next(result))
+                {
+                    var tuple = kuzu_query_result_get_next(result);
 
-                value = kuzu_flat_tuple_get_value(tuple, 2);
-                String name2 = kuzu_value_get_string(value);
-                kuzu_value_destroy(value);
+                    var value = kuzu_flat_tuple_get_value(tuple, 0);
 
-                Console.WriteLine(String.Format("{0} follows {1} since {2}", name, name2, since));
-                kuzu_flat_tuple_destroy(tuple);
+                    String name = kuzu_value_get_string(value);
+                    kuzu_value_destroy(value);
+
+                    value = kuzu_flat_tuple_get_value(tuple, 1);
+                    long since = kuzu_value_get_int64(value);
+                    kuzu_value_destroy(value);
+
+                    value = kuzu_flat_tuple_get_value(tuple, 2);
+                    String name2 = kuzu_value_get_string(value);
+                    kuzu_value_destroy(value);
+
+                    Console.WriteLine(String.Format("{0} follows {1} since {2}", name, name2, since));
+                    kuzu_flat_tuple_destroy(tuple);
+                }
+
+                kuzu_query_result_destroy(result);
+                kuzu_connection_destroy(conn);
+                kuzu_database_destroy(db);
             }
-            kuzu_query_result_destroy(result);
-            kuzu_connection_destroy(conn);
-            kuzu_database_destroy(db);
 
             Console.WriteLine("Press enter to close...");
             Console.ReadLine();
+        }
+
+        private static void PerformNonQuery(kuzu_connection conn, string query)
+        {
+            kuzu_query_result result = kuzu_connection_query(conn, query);
+            kuzu_query_result_destroy(result);
         }
     }
 }
---
+++ KuzuDB-net/KuzuDB-ConsoleCore/Program.cs
@@ -0,0 +1,64 @@
+﻿using KuzuDB;
+using System.Xml.Linq;
+using static KuzuDB.kuzunet;
+
+namespace KuzuDB_ConsoleCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kuzu_database_init("test", kuzu_default_system_config(), out var db);
+            using (db)
+            {
+                kuzu_connection_init(db, out var conn);
+                using (conn)
+                {
+                    PerformNonQuery(conn, "CREATE NODE TABLE User(name STRING, age INT64, PRIMARY KEY (name))");
+                    PerformNonQuery(conn, "CREATE NODE TABLE City(name STRING, population INT64, PRIMARY KEY (name))");
+                    PerformNonQuery(conn, "CREATE REL TABLE Follows(FROM User TO User, since INT64)");
+                    PerformNonQuery(conn, "CREATE REL TABLE LivesIn(FROM User TO City)");
+
+                    PerformNonQuery(conn, "COPY User FROM \"csv/users.csv\"");
+                    PerformNonQuery(conn, "COPY City FROM \"csv/cities.csv\"");
+                    PerformNonQuery(conn, "COPY Follows FROM \"csv/follows.csv\"");
+                    PerformNonQuery(conn, "COPY LivesIn FROM \"csv/lives.csv\"");
+
+                    PerformQuery(conn, "MATCH (a:User)-[f:Follows]->(b:User) RETURN a.name, f.since, b.name;", out var result);
+
+                    while (kuzu_query_result_has_next(result))
+                    {
+                        kuzu_query_result_get_next(result, out var tuple);
+                        using (tuple)
+                        {
+                            string name, name2;
+                            int since;
+
+                            kuzu_flat_tuple_get_value(tuple, 0, out var value);
+                            using (value) kuzu_value_get_string(value, out name);
+
+                            kuzu_flat_tuple_get_value(tuple, 1, out value);
+                            using (value) kuzu_value_get_int64(value, out since);
+
+                            kuzu_flat_tuple_get_value(tuple, 2, out value);
+                            using (value) kuzu_value_get_string(value, out name2);
+
+
+                            Console.WriteLine(String.Format("{0} follows {1} since {2}", name, name2, since));
+                        }
+                    }
+                }
+            }
+        }
+
+        private static kuzu_state PerformNonQuery(kuzu_connection conn, string query)
+        {
+            return kuzu_connection_query(conn, query, out kuzu_query_result result);
+        }
+
+        private static kuzu_state PerformQuery(kuzu_connection conn, string query, out kuzu_query_result result)
+        {
+            return kuzu_connection_query(conn, query, out result);
+        }
+    }
+}
