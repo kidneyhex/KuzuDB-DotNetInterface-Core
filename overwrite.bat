12f0095b2f6db585120799157e062d71f1181ecc
Logan B <3870583+thinkpoop@users.noreply.github.com>
2025-10-03 09:40:26 -0500

    Update SWIG-generated files to version 4.3.1 and clean up batch scripts

--- overwrite.bat
+++ overwrite.bat
@@ -4,11 +4,6 @@ REM STEP 3) overwrite.bat ** YOU ARE HERE
 
 REM This copies the newly created CSharp classes to the .NET projects
 
-del kuzudb-net\kuzudb-netcore\wrapperfiles\*.* /q
-copy KuzuFiles\generated_classes\*.* kuzudb-net\kuzudb-netcore\wrapperfiles\ /y
-copy wrapperlib\build\kuzunet.dll kuzudb-net\kuzudb-netcore\ /y
-copy KuzuFiles\kuzu_shared.dll kuzudb-net\kuzudb-netcore\ /y
-
 del kuzudb-net\kuzudb-net\wrapperfiles\*.* /q
 copy KuzuFiles\generated_classes\*.* kuzudb-net\kuzudb-net\wrapperfiles\ /y
 copy wrapperlib\build\kuzunet.dll kuzudb-net\kuzudb-net\ /y
--- refresh.bat
+++ refresh.bat
@@ -1,10 +1,9 @@
-REM STEP 1) refresh.bet ** YOU ARE HERE
-REM STEP 2) wrapperlib\rebuild.bat
+REM STEP 1) refresh.bat ** YOU ARE HERE
+REM STEP 2) rebuild.bat
 REM STEP 3) overwrite.bat
 
 REM Clear out old generated files before inserting new ones
 @del KuzuFiles\generated_classes\*.* /Q
 
 REM Get Swig from here: https://www.swig.org/download.html
-REM TODO: Update this to your path to swig
-p:\home\tools\swigwin\swig.exe -c++ -csharp -IKuzuFiles/ -outdir "KuzuFiles/generated_classes/" -o wrapperlib/kuzu_wrap.cpp SWIG-InputFile/kuzu.i
+"P:\Home\Tools\swigwin\swigwin-4.3.1\swig.exe" -c++ -csharp -IKuzuFiles/ -outdir "KuzuFiles/generated_classes/" -o wrapperlib/kuzu_wrap.cpp SWIG-InputFile/kuzu.i
